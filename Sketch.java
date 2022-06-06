bab71b315cfd2cc489f382754923fc08c42fd6ba
Evan Chan <100213223+Evan980@users.noreply.github.com>
2022-06-06 13:52:44 +0000

    background

--- Sketch.java
+++ Sketch.java
@@ -1,13 +1,15 @@
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
-	
+	public PImage img;
 	
   
   public void settings() {
 	// put your size call here
-    size(660, 400);
+    size(1600, 800);
   }
+
+  img = loadImage("cptbackground.jpg");
 
   public void setup() {
     background(210, 255, 173);
