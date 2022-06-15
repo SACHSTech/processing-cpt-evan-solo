1f211ef99f5a676c00389f0d33859507b416f903
Evan Chan <100213223+Evan980@users.noreply.github.com>
2022-06-15 23:20:44 +0000

    barriers and movement

--- Sketch.java
+++ Sketch.java
@@ -6,9 +6,11 @@ public class Sketch extends PApplet {
   // Images
 	PImage background;
   PImage background2;
-  PImage sonicRun;
+  PImage sonicRunRight;
+  PImage sonicRunLeft;
   PImage sonicAttack;
-  PImage[] sonicRunFrames;
+  PImage[] sonicRunLeftFrames;
+  PImage[] sonicRunRightFrames;
   PImage[] sonicAttackFrames;
   PImage drEggman;
   PImage[] drEggmanFrames;
@@ -32,6 +34,8 @@ public class Sketch extends PApplet {
   float playerWidth = 12;
   float playerHeight = 18;
   float playerLives = 3;
+  float playerSpeedY = 0;
+  boolean jumping = false;
 
   // Enemy
   float enemyX;
@@ -76,7 +80,9 @@ public class Sketch extends PApplet {
     background2 = spritesheet.get(16,133,295,147);
     background2.resize(width, height);
 
-    sonicRun = spritesheet.get(20,20,sonic_runFrames*sonic_runFrameWidth,90);
+    sonicRunLeft = spritesheet.get(20,20,sonic_runFrames*sonic_runFrameWidth,90);
+
+    sonicRunRight = spritesheet.get(245,563,sonic_runFrames*sonic_runFrameWidth,90);
     
     sonicAttack = spritesheet.get(5,289,374,118);
 
@@ -87,22 +93,28 @@ public class Sketch extends PApplet {
     gameOverScreen = spritesheet.get(481,299,393,216);
     gameOverScreen.resize(width, height);
 
-    // Running animation sonic
-    sonicRunFrames = new PImage[sonic_runFrames];
+    // Running animation sonic going left
+    sonicRunLeftFrames = new PImage[sonic_runFrames];
     for(int i = 0; i < sonic_runFrames; i++){
-      sonicRunFrames[i] = spritesheet.get(sonic_runFrameWidth*i, 0, sonic_runFrameWidth, drEggman.height);
+      sonicRunLeftFrames[i] = sonicRunLeft.get(sonic_runFrameWidth*i, 0, sonic_runFrameWidth, sonicRunLeft.height);
+    }
+
+    // Running animation sonic going right
+    sonicRunRightFrames = new PImage[sonic_runFrames];
+    for(int i = 0; i < sonic_runFrames; i++){
+      sonicRunRightFrames[i] = sonicRunRight.get(sonic_runFrameWidth*i, 0, sonic_runFrameWidth, sonicRunRight.height);
     }
 
     // Attack animation sonic
     sonicAttackFrames = new PImage[sonic_attackFrames];
     for(int i = 0; i < sonic_attackFrames; i++){
-      sonicAttackFrames[i] = spritesheet.get(sonic_attackFrameWidth*i, 0, sonic_attackFrameWidth, sonicAttack.height);
+      sonicAttackFrames[i] = sonicAttack.get(sonic_attackFrameWidth*i, 0, sonic_attackFrameWidth, sonicAttack.height);
     }
 
     // Run animation dr eggman
     drEggmanFrames = new PImage[drEggman_runFrames];
     for(int i = 0; i < drEggman_runFrames; i++){
-      drEggmanRunFrames[i] = spritesheet.get(drEggman_runFrameWidth*i, 0, drEggman_runFrameWidth, sonicRun.height);
+      drEggmanRunFrames[i] = drEggman.get(drEggman_runFrameWidth*i, 0, drEggman_runFrameWidth, drEggman.height);
     }
  }
     
@@ -175,8 +187,65 @@ public class Sketch extends PApplet {
 }
   
   public void sonic(){
+
+    playerY += playerSpeedY;
     //Drawing running sonic
-    image(sonicRunFrames[(frameCount/5)%sonic_runFrames], playerX, playerY);
+    if(aPressed == true  && gameover = false){   
+      image(sonicRunLeftFrames[(frameCount/5)%sonic_runFrames], playerX, playerY);
+      playerX -= speed;
+      jumping = true;
+    }
+
+    else if(dPressed == true && gameover = false){ 
+      image(sonicRunLeftFrames[(frameCount/5)%sonic_runFrames], playerX, playerY);
+      playerX += speed;
+      jumping = true;
+    }
+
+    else if(spacePressed == true && gameover = false){
+      if(!jumping){
+        playerSpeedY = -10;
+        jumping = true;
+      }
+    }
+
+    // Ground collision
+    if(playerY + playerHeight > 340 && 23 < playerX < 150){
+      playerY = 340;
+      playerSpeedY = 0;
+      jumping = false
+    }
+
+    if(playerY + playerHeight > 300 && 220 < playerX < 450){
+      playerY = 300;
+      playerSpeedY = 0
+      jumping = false
+    }
+
+    if(playerY + playerHeight > 405 && 530 < playerX < 600){
+      playerY = 405;
+      playerSpeedY = 0;
+      jumping = false;
+    }
+
+    if(playerY + playerHeight > 340 && 640 < playerX < 765){
+      playerY = 340;
+      playerSpeedY = 0;
+      jumping = false;
+    }
+
+    if(playerY + playerHeight > 385 && 850 < playerX < 969){
+      playerY = 385;
+      playerSpeedY = 0;
+      jumping = false;
+    }
+
+    if(playerY + playerHeight > 0){
+      playerLives = playerLives - 1;
+      playerX = 80;
+      playerY = 400;
+    }
+    
   }
   
 }
